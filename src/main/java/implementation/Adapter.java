c761e76cf1846165129cfc3629e0284cc4c6c0eb
juampi senmartin <juanpablo.senmartin@ing.austral.edu.ar>
2023-06-23 23:09:18 -0300

    fixing memory tests

--- src/main/java/implementation/Adapter.java
+++ src/main/java/implementation/Adapter.java
@@ -14,44 +14,36 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.util.Iterator;
-import java.util.List;
 
 
 public class Adapter {
     private final Interpreter interpreter;
-    private final PrintEmitter emitter;
     private final InputProvider provider;
 
     public Adapter(InputStream input, Double version, PrintEmitter emitter, InputProvider provider) {
-        this.interpreter = createInterpreter(input, version);
-        this.emitter = emitter;
+        this.interpreter = createInterpreter(input, version, emitter);
         this.provider = provider;
     }
 
     public void interpret(){
+        addInput();
+        interpreter.interpret();
+    }
+    public void addInput(){
         String readInput = provider.input("");
         if (readInput != null && !readInput.equals("")){
             InputStream in = new ByteArrayInputStream(readInput.getBytes());
             System.setIn(in);
         }
-        interpreter.interpret();
-
-        emitPrintedMessages(interpreter.getPrinted());
     }
 
 
-    private Interpreter createInterpreter(InputStream input, Double version) {
+    private Interpreter createInterpreter(InputStream input, Double version, PrintEmitter emitter) {
         PushbackInputStream inputStream = new PushbackInputStream(input);
         Lexer lexer = new Lexer(inputStream, version);
         PeekingIterator<Token> tokenIterator = lexer.getTokenIterator();
         Parser parser = new Parser(tokenIterator, version);
         Iterator<Node> nodeIterator = parser.getNodeIterator();
-        return new Interpreter(nodeIterator);
-    }
-
-    public void emitPrintedMessages(List<String> toPrint){
-        for (String message : toPrint){
-            emitter.print(message);
-        }
+        return new Interpreter(nodeIterator, emitter::print);
     }
 }
